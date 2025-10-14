615062f3a421f7d32a4acaaab121db2f61a1e132
Matthieu FELICITE <matthieufelicite@icloud.com>
2025-10-14 14:37:52 +0200

    initial commit

---
+++ src/main/java/com/pam/asta/AstaApplication.java
@@ -0,0 +1,13 @@
+package com.pam.asta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pam/asta/AstaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pam.asta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
