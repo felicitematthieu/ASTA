19349171af907597caa96caaa1201d3615010dfe
Matthieu FELICITE <matthieufelicite@icloud.com>
2025-10-30 22:51:53 +0100

    corrections '

--- src/main/java/com/pam/asta/modele/TuteurEnseignantRepository.java
+++
@@ -1,6 +0,0 @@
-package com.pam.asta.modele;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TuteurEnseignantRepository extends JpaRepository<TuteurEnseignant, Integer> {
-}
